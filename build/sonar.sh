45cda0aadffd2303f9ed75f2adb68cbbf339ca26
JAMES MURPHY <jammurp@us.ibm.com>
2025-04-02 12:51:03 -0700

    switch to new sonarqube server (#41)

--- build/sonar.sh
+++ build/sonar.sh
@@ -3,7 +3,15 @@ set -x
 
 echo "Downloading sonar-scanner version ${SONAR_SCANNER_VERSION}"
 wget -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_SCANNER_VERSION}.zip
-unzip sonar-scanner-cli-${SONAR_SCANNER_VERSION}.zip -d . 
+unzip sonar-scanner-cli-${SONAR_SCANNER_VERSION}.zip -d .
 
 # Scanning repo based on change being a pull request or branch scan
-./sonar-scanner-${SONAR_SCANNER_VERSION}/bin/sonar-scanner -Dsonar.projectKey=${IMAGE_NAME} -Dsonar.sources=. -Dsonar.exclusions=**/*test* -Dsonar.host.url=http://cp4d-sonarqube.svl.ibm.com:9000/sonar -Dsonar.branch.name=${BRANCH} -Dsonar.login=${SONAR_KEY} 
+./sonar-scanner-${SONAR_SCANNER_VERSION} -D sonar.projectKey=${PROJECT_KEY} \
+                                         -D sonar.projectName="${IMAGE_NAME}" \
+                                         -D sonar.sources=. \
+                                         -D sonar.exclusions=**/*test*,**/lock-file.c \
+                                         -D sonar.host.url=${SONARQUBE_SERVER} \
+                                         -D sonar.login=${SONAR_KEY} \
+                                         -D sonar.branch.name=${BRANCH} \
+                                         -D sonar.scanner.truststorePath=./build/sonar_cert \
+                                         -D sonar.scanner.truststorePassword=${SONAR_CERT_PW}
