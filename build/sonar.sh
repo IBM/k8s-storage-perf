9af0ffbaac7ebfdd2822fc05e514ad3bf2f40891
JAMES MURPHY <jammurp@us.ibm.com>
2025-04-08 10:35:39 -0700

    update sonar project name (#44)

--- build/sonar.sh
+++ build/sonar.sh
@@ -8,7 +8,7 @@ unzip sonar-scanner-cli-${SONAR_SCANNER_VERSION}.zip -d .
 # Scanning repo based on change being a pull request or branch scan
 ./sonar-scanner-${SONAR_SCANNER_VERSION}/bin/sonar-scanner \
      -D sonar.projectKey=${PROJECT_KEY} \
-     -D sonar.projectName="${IMAGE_NAME}" \
+     -D sonar.projectName="${JOB_ORGANIZATION}/${GHE_REPO}" \
      -D sonar.sources=. \
      -D sonar.exclusions=**/*test*,**/lock-file.c \
      -D sonar.host.url=${SONARQUBE_SERVER} \
